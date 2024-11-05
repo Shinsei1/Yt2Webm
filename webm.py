ea22993d67b39fd9741a62e869b32ea0e1bda793
Shinsei1 <116077273+Shinsei1@users.noreply.github.com>
2024-11-05 15:36:12 +0100

    Update webm.py
    added tkinter, cleared the file

--- webm.py
+++ webm.py
@@ -1,10 +1,11 @@
 import yt_dlp
 import os 
+from tkinter import filedialog
 
 def download_youtube_audio(video_url):
-    # Options pour le téléchargement
 
-    output_directory = "C:\\audios\\"  
+    output_directory = filedialog.askdirectory(title="Select a folder to save your file in")
+    
     os.makedirs(output_directory, exist_ok=True) 
 
     ydl_opts = {
@@ -12,10 +13,7 @@ def download_youtube_audio(video_url):
         'outtmpl': os.path.join(output_directory,'%(title)s.%(ext)s'),  # Nom du fichier de sortie
     }
 
-    # Téléchargement avec yt-dlp
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
         ydl.download([video_url])
 
-#Exemple 
-#video_url = 'https://www.youtube.com/watch?v=2zdnd2PD_l0&list=LL&index=2'  
-#download_youtube_audio(video_url)
+
